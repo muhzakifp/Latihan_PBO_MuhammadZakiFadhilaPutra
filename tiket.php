7ea8f6bb83d9c3a1dc7fd60a4efbe5bbf8a596c1
jaki <mzakifadhila@gmail.com>
2026-06-17 15:20:10 +0700

    [Tahap 3] membuat abstrak class ticket

---
+++ tiket.php
@@ -0,0 +1,31 @@
+<?php
+
+abstract class Tiket{
+    protected $id_tiket;
+    protected $nama_tiket;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $hargaDasarTiket;
+
+    public function __construct($id_tiket,$nama_tiket,$jadwal_tayang,$jumlah_kursi,$hargaDasarTiket ){
+        $this->id_tiket = $id_tiket;
+        $this->nama_tiket = $nama_tiket;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->hargaDasarTiket = $hargaDasarTiket;
+    }
+
+    //method getter
+    public function getIdTiket(){return $this->id_tiket;}
+    public function getnamaTiket(){return $this->nama_tiket;}
+    public function getjadwalTayang(){return $this->jadwal_tayang;}
+    public function getjumlahKursi(){return $this->jumlah_kursi;}
+    public function gethargaDasarTiket(){return $this->hargaDasarTiket;}
+
+    //method abstrak
+    abstract function hitungTotalHarga();
+    abstract function tampilkanInfoFasilitas();
+
+}
+
+?>
